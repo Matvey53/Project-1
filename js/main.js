73b62f2c3c42225acb6e65d1e4dde748f53473ae
Matvey53 <mateusvip1012@gmail.com>
2023-01-19 18:46:19 +0300

    menu

--- js/main.js
+++ js/main.js
@@ -159,36 +159,10 @@ class ItcCustomSelect {
 ItcCustomSelect.hideOpenSelect();
 
 
-
-const callRequestDialog = document.querySelector('#call-request-dialog');
-const callRequestBtn = document.querySelector('#call-request');
-const callRequestClose = callRequestDialog.querySelector('#call-request-close');
-const callRequestOverlay = callRequestDialog.querySelector('#call-request-overlay');
-
-function handleOpenRequestDialog() {
-    callRequestDialog.classList.add('dialog_active');
-}
-
-function handleCloseRequestDialog() {
-    callRequestDialog.classList.remove('dialog_active');
-}
-
-callRequestBtn.addEventListener('click', handleOpenRequestDialog);
-callRequestClose.addEventListener('click', handleCloseRequestDialog);
-callRequestOverlay.addEventListener('click', handleCloseRequestDialog);
-
-
-
-
-
-
-$(function () {
-    $('.mobile-menu__icon').click(function () {
-        if ($('.header__navigation').is(':visible')) {
-            $('.header__navigation').removeClass('showitems');
-        }
-        else {
-            $('.header__navigation').addClass('showitems');
-        }
+$(document).ready(function () {
+    $('.hamb').click(function (event) {
+        $('.mobile-menu,.header__navigation').toggleClass('active');
     });
 });
+
+
